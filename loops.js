c11dc57ea44ad4322467d9c1f7eda0af50d6c9cf
Fin <fin.tsai86@gmail.com>
2024-10-29 17:02:51 +1100

    Loops

---
+++ loops.js
@@ -0,0 +1,20 @@
+//Loops
+//The For Loop
+
+/*Create a program that loops from 5 to 10 and logs each number to the console.*/
+for (let number = 5; number <= 10; number++)
+  {
+    console.log(number);
+  }
+  
+
+//Looping in Reverse
+for (let counter = 3; counter >= 0; counter--){
+  console.log(counter);
+}
+//print 3 2 1 0
+
+
+//Looping through Arrays
+
+
