2234639953eacaf92ef7dc3fb6f76760f1d60d3f
Fin <fin.tsai86@gmail.com>
2025-03-11 15:27:47 +1100

    add Methods

--- CreatingObjectLiterals.js
+++ CreatingObjectLiterals.js
@@ -77,3 +77,32 @@ Output:
   */
   
 
+
+
+
+  //Methods
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+// Write your code below
+let alienShip = {
+  retreat() {
+    console.log(retreatMessage);
+  },
+  takeOff() {
+    console.log('Spim... Borp... Glix... Blastoff!');
+  }
+  //separate your methods with commas just as you would any other key-value pairs
+};
+
+alienShip.retreat();
+alienShip.takeOff();
+
+/*print
+
+Output-only Terminal
+Output:
+We no longer wish to conquer your planet. It is full of dogs, which we do not care for.
+Spim... Borp... Glix... Blastoff!
+*/
+
+
