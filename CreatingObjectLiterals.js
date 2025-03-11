b8508c07b71c949af1c545b003ed02763ed5e930
Fin <fin.tsai86@gmail.com>
2025-03-11 14:33:42 +1100

    add Bracket Notation

--- CreatingObjectLiterals.js
+++ CreatingObjectLiterals.js
@@ -28,3 +28,23 @@ let crewCount = spaceship.numCrew;
 let planetArray = spaceship.flightPath;
 
 console.log(crewCount, planetArray);
+
+
+
+
+//Bracket Notation
+/*We must use bracket notation when accessing keys that have numbers, spaces, or special characters in them. Without bracket notation in these situations, our code would throw an error.*/
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  'Active Mission' : true,
+  homePlanet : 'Earth', 
+  numCrew: 5
+ };
+
+let propName =  'Active Mission';
+
+// Write your code below
+let isActive = spaceship['Active Mission'];
+console.log(isActive);
+
