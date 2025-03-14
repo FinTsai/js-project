4af961f339e86fe30232455eb90e7eb9114e1f50
Fin <fin.tsai86@gmail.com>
2025-03-14 14:06:00 +1100

    add Arrow Functions and this

--- TheThisKeyword.js
+++ TheThisKeyword.js
@@ -11,3 +11,20 @@ console.log(robot.provideInfo());
 /*print
 I am 1E78V2 and my current energy level is 100.
 */
+
+
+
+
+
+
+//Arrow Functions and this
+const robot = {
+  energyLevel: 100,
+  checkEnergy () {
+    //The key takeaway from the example above is to avoid using arrow functions when using this in a method!
+    //use a function expression
+    console.log(`Energy is currently at ${this.energyLevel}%.`)
+  }
+}
+
+robot.checkEnergy();
