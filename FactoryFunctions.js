c03aaf489e01c2d8735f554bd7bf274e0b24efab
Fin <fin.tsai86@gmail.com>
2025-03-17 15:41:47 +1100

    add Factory Functions

---
+++ FactoryFunctions.js
@@ -0,0 +1,29 @@
+/*example
+const monsterFactory = (name, age, energySource, catchPhrase) => {
+  return { 
+    name: name,
+    age: age, 
+    energySource: energySource,
+    scare() {
+      console.log(catchPhrase);
+    } 
+  }
+};
+const ghost = monsterFactory('Ghouly', 251, 'ectoplasm', 'BOO!');
+ghost.scare(); // 'BOO!'
+*/
+
+
+const robotFactory = (model, mobile) => {
+  return {
+    model : model,
+    mobile : mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+};
+
+const tinCan = robotFactory('P-500', true);
+tinCan.beep();
+//print Beep Boop
