4b01f7f39698ddb5e55e4c7a41b36f6bef87df5e
Fin <fin.tsai86@gmail.com>
2025-03-17 13:48:55 +1100

    add Privacy

--- TheThisKeyword.js
+++ TheThisKeyword.js
@@ -28,3 +28,34 @@ const robot = {
 }
 
 robot.checkEnergy();
+
+
+
+
+
+//Privacy
+//place an underscore _ before the name of a property to mean that the property should not be altered.
+/*example of using _ to prepend a property.
+const bankAccount = {
+  _amount: 1000
+}
+
+
+//Even so, it is still possible to reassign _amount:
+bankAccount._amount = 1000000;
+
+*/
+
+const robot = {
+  _energyLevel: 100,
+  recharge(){
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+  }
+};
+
+robot._energyLevel = 'high';
+
+robot.recharge();
+//print Recharged! Energy is currently at high30%.
+
