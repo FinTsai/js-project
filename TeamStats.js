01eb38331432158079b59a8140616f429c574d8a
Fin <fin.tsai86@gmail.com>
2025-03-24 12:42:10 +1100

    Adding Data in Team stats

--- TeamStats.js
+++ TeamStats.js
@@ -5,14 +5,51 @@ After we create these data structures in this project, feel free to challenge yo
 */
 
 const team = {
-  _players: ["firstName", "lastName", "age"],
-  _games: ["opponent", "teamPoints", "opponentPoints"],
+  _players: [
+    { firstName: 'John', lastName: 'Don', age: 28 },
+    {firstName: 'Merry', lastName: 'li', age: 35},
+    {firstName: 'Hana', lastName: 'Yu', age: 45}
+    ],
+  _games: [
+    { opponent: 'Mermaid', teamPoints: 32, opponentPoints: 12},
+    { opponent: 'Fish', teamPoints: 23, opponentPoints: 9},
+    { opponent: 'Unicorn', teamPoints: 46, opponentPoints: 31}
+    ],
   get players() {
     return this._players
     //or return team.players
   },
   get games() {
     return this._games
-  }
+  },
+  addPlayer(newFirstName, newLastName, newAge) {
+  const player = {
+    firstName: newFirstName,
+    lastName: newLastName,
+    age: newAge
+  };
+  this._players.push(player);
+  },
+  addGame(newOpponent, newTeamPoints, newOpponentPoints) {
+  const game = {
+    opponent: newOpponent,
+    teamPoints: newTeamPoints,
+    opponentPoints: newOpponentPoints
+  };
+  this._games.push(game);
+}
 };
+/*We want to add a new player to our team. Add a .addPlayer() method to the team object. This method should take in three parameters: newFirstName, newLastName, and newAge.
+
+Inside the method, create a player object by setting the three parameters to be the values for the object’s three properties: firstName, lastName, age. Finally, add the player object to the team‘s _players array.
+*/
+
+
+team.addPlayer('Bugs', 'Bunny', 76);
+console.log(team.players);
+team.addGame('Titans', 100, 98);
+console.log(team.games);
+
+
+
 
